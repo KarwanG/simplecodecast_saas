6df117fd697e7f1ddfc58b617b3247c73409975a
Karwan Gully <karwangully@gmail.com>
2015-04-18 12:08:19 +0000

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-  
+  def new
+    # form for where a user can file out theri own profile
+    @user = User.find( params[:user_id] )
+    @variable = params[:hello]
+    @profile = @user.build_profile
+  end
 end
